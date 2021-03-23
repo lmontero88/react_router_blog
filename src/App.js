88ede0a18dc4082ffa4b05ff164a490242a67553
Lilian Montero <lilianmonterolopez@gmail.com>
2021-03-23 00:09:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <h1>
+            Mi blog personal
+         </h1>
+          <nav>
+            <Link to="/">Inicio</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/acerca_mi">Acerca de </Link>
+          </nav>
+        </header>
+        <main>
+          <Switch> 
+            <Route path='/' exact={true}>
+              <div>
+                <h2>
+                  Pagina de Inicio
+          </h2>
+                <p> Esta es la pagina principal</p>
+              </div>
+            </Route>
+            <Route path='/blog'>
+              <div>
+                <h2>
+                  BLog
+              </h2>
+                <ul>
+                  <li>Articulo 1 </li>
+                  <li>Articulo 2</li>
+                  <li>Articulo 3</li>
+                </ul>
+              </div>
+            </Route>
+            <Route path='/acerca_mi'>
+              <div>
+                <h2>
+                  Acerca de
+          </h2>
+                <p> Este soy yo</p>
+              </div>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </BrowserRouter>
+
+
   );
 }
 
